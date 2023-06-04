71191971742cbf526cb7984641229c0584f54d50
t.schoenijahn <t.schoenijahn@m3connect.de>
2023-06-04 13:28:04 +0200

    restructure Valve class

--- controller/src/helpers/valve.py
+++ controller/src/helpers/valve.py
@@ -5,6 +5,12 @@ import time
 class Valve:
     _pin = Pins.VALVE
     def enablePeriod(self):
-        self._pin.off()
+        self.enable()
         time.sleep(VALVE_TIME)
+        self.disable()
+
+    def enable(self):
+        self._pin.off()
+
+    def disable(self):
         self._pin.on()
