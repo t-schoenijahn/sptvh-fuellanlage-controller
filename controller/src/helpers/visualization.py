f70963664dfe76984222bd9945f3f36e3117c79b
t.schoenijahn <t.schoenijahn@m3connect.de>
2023-06-14 11:37:24 +0200

    add RGB led to visualization

--- controller/src/helpers/visualization.py
+++ controller/src/helpers/visualization.py
@@ -1,6 +1,8 @@
 from static.definitions import State
 from libraries.display import Display
-from config import *
+from libraries.rgb_led import RgbLed
+from config import Pins, colorCodes, messages
+import time
 
 
 class Visualzation:
@@ -8,10 +10,13 @@ class Visualzation:
 
     def __init__(self) -> None:
         self._lcd = Display(Pins.DISPLAY_SDA, Pins.DISPLAY_SCL)
+        self._rgb = RgbLed(Pins.RGB_R, Pins.RGB_G, Pins.RGB_B)
 
-    def showData(self, state: int, text1="", text2="", values=""):
+    def showData(self, state: int, text1="", text2="", values="", sleep = 0):
         self._lcd.setLight(True)
+        self._rgb.showColor(colorCodes[state])
         self._lcd.writeText([messages[state], text1, text2, values])
+        time.sleep(sleep)
 
     def off(self):
         self._lcd.setLight(False)
